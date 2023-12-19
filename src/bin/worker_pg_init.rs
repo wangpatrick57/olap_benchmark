79e0655700e3951c8e7456243c0296f38e652280
Patrick Wang <wang.patrick57@gmail.com>
2023-12-19 15:32:33 -0800

    migrated pg_init doc comment

---
+++ src/bin/worker_pg_init.rs
@@ -0,0 +1,19 @@
+/**
+ * # Run Environment
+ * Worker node
+ * 
+ * # Arguments
+ *  * `pgdata_dpath` - The data directory for this Postgres instance
+ *  * `pgbin_dpath` - The bin directory with initdb
+ * 
+ * # Preconditions
+ *  * `pgdata_dpath` is non-existent
+ *  * `pgbin_dpath` is a built Postgres bin
+ * 
+ * # Postconditions
+ *  * `initdb` will be called on `pgdata_dpath`
+ * 
+ * # Notes
+ *  * `worker_pg_start` is separate from `worker_pg_init` because start happens every time a node crashes and comes back but init only happens once
+ */
+fn main() {}
