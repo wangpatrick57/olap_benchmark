b3cddda85db06f50eaaaff5f94109f566aa25566
Patrick Wang <wang.patrick57@gmail.com>
2023-12-19 15:46:25 -0800

    small comment changes

--- src/bin/worker_pg_init.rs
+++ src/bin/worker_pg_init.rs
@@ -1,6 +1,6 @@
 /**
- * # Run Environment
- * Worker node
+ * # Environment
+ * Run on worker nodes
  * 
  * # Arguments
  *  * `pgdata_dpath` - The data directory for this Postgres instance
@@ -16,4 +16,6 @@
  * # Notes
  *  * `worker_pg_start` is separate from `worker_pg_init` because start happens every time a node crashes and comes back but init only happens once
  */
-fn main() {}
+fn main() {
+    
+}
